d1990078eaa1a82c8cc6486fe5c53916da924aae
Michael Beirne <133065459+michaelbeirnee@users.noreply.github.com>
2026-06-15 21:27:41 -0400

    fix

--- src/main/java/com/example/todo/todo/TodoController.java
+++ src/main/java/com/example/todo/todo/TodoController.java
@@ -1,5 +1,33 @@
 package main.java.com.example.todo.todo;
 
+//tells spring to check validation rules
+import jakarata.validation.Valid; 
+
+//spring web - gives https satus codes 
+import org.springframework.http.HttpStatus; 
+
+//spring web - handles delete requests
+//explain how mapping from spring works
+import org.springframework.web.bind.annotation.DeleteMapping; 
+
+//spring web - handle get requests
+import org.springframework.web.bind.annotation.GetMapping; 
+
+//spring web - reads values form the url path
+import org.springframework.web.bind.annotation.PathVariable; 
+
+//handles post requests 
+import org.springframwork.web.bind.annotation.PostMapping; 
+
+//handles our put requests 
+//is this REST API? 
+import org.springframework.web.bind.annotation.PutMapping; 
+
+//spring web - reads json from the request body 
+import org.springframework .web.bind.annotatin.RequestBody; 
+
+
+
 public class TodoController {
     
 }
